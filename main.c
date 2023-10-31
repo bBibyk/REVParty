ba1d8b23d5f4d091e6990d2248ca40fcd8a25f0a
bBibyk <bbibyk@gmail.com>
2023-10-31 18:10:35 +0100

    main credits comments

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/**
+ * @file main.c
+ * @brief //TODO brief
+ * @author Bibyk Bogdan
+ * @date 31 octobre 2023
+ *
+ * //TODO
+ *
+ */
 
+#include "dynamic_array.h"
+#include "condorcet.h"
+#include "jugement_majoriaire.h"
+#include "uninominales.h"
+//TODO : includes
 
-#include "utils_sd.h"
+//TODO : code avec commentaires et division en parties
