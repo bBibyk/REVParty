d4eef647d5260afe58b47900cdaa830be1c3ad71
bBibyk <bbibyk@gmail.com>
2023-10-31 18:02:04 +0100

    Création des modules

---
+++ condorcet.h
@@ -0,0 +1,19 @@
+/**
+ * @file condorcet.h
+ * @brief //TODO (en-tête).
+ * @author Wassim Chenouf
+ * @date 31 octobre 2023
+ *
+ * //TODO
+ *
+ */
+
+#ifndef CONDORCET_H
+#define CONDORCET_H
+
+#include "dynamic_array.h"
+//TODO : includes
+
+//TODO : en-têtes de fonctions
+
+#endif
