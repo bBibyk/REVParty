60c8d6877ce105b52f45487fb23f4f2e0746918b
bBibyk <bbibyk@gmail.com>
2023-11-21 13:11:28 +0100

    tests 21-13:11

--- src/main.c
+++ src/main.c
@@ -11,22 +11,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
 
 #include "lecture_csv.h"
 #include "condorcet.h"
 #include "jugement_majoritaire.h"
 #include "uninominales.h"
-//TODO : includes
 
 ////////////////////////////////////////////////////////
 // -- Fonctions de gestion de la ligne de commande -- //
 ////////////////////////////////////////////////////////
 
+//To be removed
+
+//End to be removed
+
+char *getParameters(int argc, char *argv[]){
+}
+
+void checkParameters();
+
 int main(int argc, char *argv[])
 {
-    int f;
-    f = getopt(argc, argv, "i:d:om:");
-    printf("%c - %s\n", f, optarg);
-    f = getopt(argc, argv, "i:d:om:");
-    printf("%c - %s\n", f, optarg);
+    bool duel = false;
+    char inputFile[MAXCHAR];
+    char logFile[MAXCHAR];
+    char method[MAXCHAR];
+
+    int option;
+    while((option = getopt(argc, argv, "i:d:o:m:")) != -1){
+        switch(option){
+            case 'i':
+                duel = false;
+                strcpy(inputFile, optarg);
+            case 'd':
+                duel = true;
+                strcpy(inputFile, optarg);
+            case 'o':
+                strcpy(logFile, optarg);
+            case 'm':
+                strcpy(method, optarg);
+            case '?':
+                fprintf(stderr, "Usage: -i|-d nom_fichier [-o nom_fichier] -m méthode\n");
+                // exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Input is:\n%s > %i\n%s\n%s\n", inputFile, duel, logFile, method);
 }
