14bc9e792c2a85e8948f10150c51ffab6c8f9824
EmanueleP02 <pigani.emanuele@gmail.com>
2023-11-21 16:30:02 +0100

    possible definition de jugement_majoritaire.h, à revoir plus tard

--- src/jugement_majoritaire.h
+++ src/jugement_majoritaire.h
@@ -1,19 +1,24 @@
-/**
- * @file jugement_majoritaire.h
- * @brief //TODO (en-tête).
- * @author Pigani Emanuele
- * @date 31 octobre 2023
- *
- * //TODO
- *
- */
-
 #ifndef JUGEMENT_MAJORITAIRE_H
 #define JUGEMENT_MAJORITAIRE_H
 
 #include "lecture_csv.h"
-//TODO : includes
 
-//TODO : en-têtes de fonctions
+/**
+ * @file jugement_majoritaire.h
+ * @brief Gestion de la méthode de jugement majoritaire.
+ * @author Pigani Emanuele
+ * @date 31 octobre 2023
+ *
+ * Ce fichier d'en-tête définit les fonctions nécessaires pour effectuer un vote
+ * selon la méthode de jugement majoritaire et stocker les résultats dans la structure VoteResult.
+ */
+
+/**
+ * @fn VoteResult voteJugementMajoritaire(DataFrame *df);
+ * @brief Fonction pour effectuer un vote selon la méthode de jugement majoritaire.
+ * @param[in] df Pointeur vers le DataFrame contenant les résultats du vote.
+ * @return Structure VoteResult contenant les résultats du vote selon la méthode de jugement majoritaire.
+ */
+VoteResult voteJugementMajoritaire(DataFrame *df);
 
 #endif
